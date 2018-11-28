4883c5d6cc6c78e99b1e35186d19613e44a525b5
StijnDefour <defourstijn10@gmail.com>
2018-11-28 21:27:34 +0100

    android project aanmaken

---
+++ TheArtifact/app/src/main/java/clyde/and/bonie/theartifact/MainActivity.java
@@ -0,0 +1,13 @@
+package clyde.and.bonie.theartifact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
