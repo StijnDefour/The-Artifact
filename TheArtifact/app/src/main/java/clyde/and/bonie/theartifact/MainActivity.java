0f6eb01c4ea1986284159a5d8a45c8a9f74f3a51
StijnDefour <defourstijn10@gmail.com>
2018-11-29 13:22:59 +0100

    Json api

--- TheArtifact/app/src/main/java/clyde/and/bonie/theartifact/MainActivity.java
+++ TheArtifact/app/src/main/java/clyde/and/bonie/theartifact/MainActivity.java
@@ -1,9 +1,13 @@
 package clyde.and.bonie.theartifact;
 
 import android.app.Dialog;
+import android.os.AsyncTask;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.JsonToken;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +17,17 @@ import com.auth0.android.authentication.AuthenticationException;
 import com.auth0.android.provider.AuthCallback;
 import com.auth0.android.provider.WebAuthProvider;
 import com.auth0.android.result.Credentials;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,8 +73,61 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("test", "-------------------------------");
                         Log.d("test", credentials.getAccessToken());
 
-                        
+                        AsyncTask.execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Create URL
+                                try {
+                                    URL apiEndpoint = new URL("https://htf2018.now.sh/users");
+
+                                    // Create connection
+                                    HttpsURLConnection myConnection =
+                                            (HttpsURLConnection) apiEndpoint.openConnection();
+                                    myConnection.addRequestProperty("Authorization", "Bearer " + credentials.getAccessToken());
+
+                                    if (myConnection.getResponseCode() == 200) {
+                                        Log.d("test", "gelukt");
+
+                                        InputStream responseBody = myConnection.getInputStream();
+                                        InputStreamReader responseBodyReader =
+                                                new InputStreamReader(responseBody, "UTF-8");
+                                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+
+                                        
+
+                                        /*jsonReader.beginArray();
+                                        while (jsonReader.hasNext()) {
+                                            jsonReader.beginObject();
+                                            while (jsonReader.hasNext()) {
+                                                String name = jsonReader.toString();
+                                                String email = "";
+                                                if (name.equals("email")) {
+                                                    email = name;
+                                                } else {
+                                                    jsonReader.skipValue();
+                                                }
+                                                Log.d("test", name);
+                                            }
+                                            jsonReader.endObject();
+                                        }
+                                        jsonReader.endArray();
+
+                                        jsonReader.close();*/
+                                        myConnection.disconnect();
+                                    } else {
+                                        Log.d("test", "error");
+                                    }
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                            }
+                        });
                     }
                 });
     }
+
+
 }
