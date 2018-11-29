f616b9ead69819a95b4d929391598d5d0df13255
StijnDefour <defourstijn10@gmail.com>
2018-11-29 15:07:17 +0100

    Api call werkt

---
+++ TheArtifact/app/src/main/java/clyde/and/bonie/theartifact/OkHttpCall.java
@@ -0,0 +1,72 @@
+package clyde.and.bonie.theartifact;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class OkHttpCall {
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpClient client = new OkHttpClient();
+
+    private String responseStr = "";
+
+    enum RequestStatus {
+        Successful,
+        Unsuccessful,
+        Undefined
+    }
+
+    private RequestStatus status = RequestStatus.Undefined;
+
+    private Call get(String url, String token, Callback callback) {
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Authorization", "Bearer " + token)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+    void get(String route, String token) {
+        OkHttpCall call = new OkHttpCall();
+        Call myCall = call.get("https://htf2018.now.sh/" + route, token, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                status = RequestStatus.Unsuccessful;
+            }
+
+            @Override
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    responseStr = response.body().string();
+                    Log.d("test", "Successful response: " + responseStr);
+                    status = RequestStatus.Successful;
+
+                    try {
+                        JSONArray jsonArray = new JSONArray(responseStr);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject explrObject = jsonArray.getJSONObject(i);
+                            Log.d("test",explrObject.getString("email"));
+                        }
+                    } catch (JSONException e) {}
+
+                } else {
+                    status = RequestStatus.Unsuccessful;
+                }
+            }
+        });
+    }
+}
