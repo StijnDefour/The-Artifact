b65563c64d4ae74f67ac8da8d449431161b6f343
StijnDefour <defourstijn10@gmail.com>
2018-11-29 10:34:14 +0100

    QR scanner class

---
+++ TheArtifact/app/src/main/java/clyde/and/bonie/theartifact/QrScanner.java
@@ -0,0 +1,37 @@
+package clyde.and.bonie.theartifact;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class QrScanner {
+
+    public void startQRScan(Activity activity) {
+        IntentIntegrator integrator = new IntentIntegrator(activity);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(true);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+
+    public String QrOnActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("Create Report", "Canceled scan");
+                return "cancelled";
+            } else {
+                Log.d("Create Report", result.getContents());
+                return result.getContents();
+            }
+        } else {
+            return "null";
+        }
+    }
+}
